7f17424bda3dc299faaa8d38f2fbe787e22a7bba
Luis Metzger <luismetzger@me.com>
2016-07-18 22:34:57 -0600

    Finished results map design with auto location. Updated design elements and got things ready for db setup with heroku.

--- public/javascript/js/app.js
+++ public/javascript/js/app.js
@@ -161,6 +161,41 @@ function initMap() {
     styles: styleArray,
     zoom: 16
   });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You are here :)');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+        // Create a marker and set its position.
+          var marker = new google.maps.Marker({
+            map: map,
+            position: {lat: 39.757755, lng: -105.007613},
+            title: 'Hello World!'
+          });
+
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
 }
 
 initMap();
